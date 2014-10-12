ca33ce0bcbcb9266d566a3e2810b0495069d3c16
Tabofa <tabofa@gmail.com>
2014-10-12 21:11:57 +0200

    b.0.1 Första uppladdningen

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,6 @@
+import java.util.*;
 
 public class Card {
-	
 	public final static int SPADES = 0;
 	public final static int HEARTS = 1;
 	public final static int DIAMONDS = 2;
@@ -21,11 +21,63 @@ public class Card {
 	}
 	
 	public Card(int theValue, int theSuit) {
-		if (theSuit != SPADES && theSuit != HEARTS && theSuit != DIAMONDS && theSuit != CLUBS && theSuit != JOKER)
-			throw new IllegalArgumentException("Illegal playingcard suit.");
-		if (theSuit != JACK && (theValue < 1 || theValue > 13))
-			throw new IllegalArgumentException("Illegal playing card value.");
+		if(theSuit != SPADES && theSuit != HEARTS && theSuit != DIAMONDS && theSuit != CLUBS && theSuit != JOKER)
+			throw new IllegalArgumentException("Your suit is invalid.");
+		if(theSuit != JOKER && (theSuit < 1 || theSuit > 13))
+			throw new IllegalArgumentException("Your value is invalid.");
 		value = theValue;
 		suit = theSuit;
 	}
+	
+	public int getSuit() {
+		return suit;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public String getSuitAsString() {
+		switch (suit) {
+		case SPADES:	return "Spades";
+		case HEARTS: 	return "Hearts";
+		case DIAMONDS:	return "Diamonds";
+		case CLUBS:		return "Clubs";
+		default:		return "Joker";
+		}
+	}
+	
+	public String getValueAsString() {
+		if (suit == JOKER)
+			return "" + value;
+		else {
+			switch ( value ) {
+			case 1:		return "Ace";	
+			case 2:		return "2";
+			case 3:		return "3";
+			case 4:		return "4";
+			case 5:		return "5";
+			case 6:		return "6";
+			case 7:		return "7";
+			case 8:		return "8";
+			case 9:		return "9";
+			case 10:	return "10";
+			case 11:	return "Jack";
+			case 12:	return "Queen";
+			default:	return "King";
+			}
+		}
+	}
+	
+	public String toString() {
+		if(suit == JOKER) {
+			if(value == 1)
+				return "Joker";
+			else
+				return "Joker #" + value;
+		}
+		else 
+			return getValueAsString() + " of " + getSuitAsString();
+	}
+	
 }
--- src/Hand.java
+++ src/Hand.java
@@ -1,11 +1,47 @@
+import java.util.ArrayList;
+
 
 public class Hand {
 	
-	public void removeCard() {
+	private ArrayList<Card> hand;
+	
+	public Hand() {
+		hand = new ArrayList<Card>();
+	}
+	
+	public void clear() {
+		hand.clear();
+	}
+	
+	public void addCard(Card cardName) {
+		if(cardName == null)
+			throw new NullPointerException("Can't add null card to hand.");
+		hand.add(cardName);
+	}
+	
+	public void removeCard(Card cardName) {
+		hand.remove(cardName);
+	}
+	
+	public void removeCard(int cardPosition) {
+		if(cardPosition < 0 || cardPosition >= hand.size())
+			throw new IllegalArgumentException("Position does not exsist in hand: " + cardPosition);
+		hand.remove(cardPosition);
+	}
+	
+	public int getCardCount() {
+		return 1;
+	}
+	
+//	public Card getCard() {
+//
+//	}
+	
+	public void sortBySuit() {
 		
 	}
-
-	public void addCard() {
+	
+	public void sortByValue() {
 		
 	}
 }
