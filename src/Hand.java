462ad4d72b51312d62500cc0cd869ab309c5a31c
Tabofa <tabofa@gmail.com>
2014-10-13 14:19:53 +0200

    Class.Hand done

--- src/Hand.java
+++ src/Hand.java
@@ -33,15 +33,49 @@ public class Hand {
 		return hand.size();
 	}
 	
-//	public Card getCard() {
-//
-//	}
+	public Card getCard(int position) {
+		if(position < 0 || position >= hand.size())
+			throw new IllegalArgumentException("Position does not exist in hand: " 	
+					+ position);
+		return hand.get(position);
+	}
 	
 	public void sortBySuit() {
-		
+		ArrayList<Card> newHand = new ArrayList<Card>();
+		while(hand.size() > 0) {
+			int pos = 0;
+			Card c = hand.get(0);
+			for (int i = 1; i < hand.size(); i++) {
+				Card c1 = hand.get(i);
+				if(c1.getSuit() < c.getSuit() || (
+						c1.getSuit() == c.getSuit() && c1.getSuit() < c.getSuit())) {
+					pos = i;
+					c = c1;
+				}
+			}
+			hand.remove(pos);
+			newHand.add(c);
+		}
+		hand = newHand;
 	}
 	
 	public void sortByValue() {
+		ArrayList<Card> newHand = new ArrayList<Card>();
+		while( hand.size() > 0 ) {
+			int pos = 0;
+			Card c = hand.get(0);
+			for (int i = 1; i < hand.size(); i++) {
+				Card c1 = hand.get(i);
+				if ( c1.getValue() < c.getValue() ||
+						(c1.getValue() == c.getValue() && c1.getSuit() < c.getSuit())) {
+					pos = i;
+					c = c1;
+				}
+			}
+			hand.remove(pos);
+			newHand.add(c);
+		}
+		hand = newHand;
 		
 	}
 }
