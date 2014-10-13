caab9772c0849cead046d6fce2c7cde70ec11c74
Jonas Hall <tabofa@gmail.com>
2014-10-13 21:06:20 +0200

    Added class BlackJack.java for the ability to play Black Jack (21)!

---
+++ src/BlackJack.java
@@ -0,0 +1,15 @@
+
+public class BlackJack {
+	
+    int gamesPlayed = 0;     // Number of games user has played.
+    int sumOfScores = 0;     // The sum of all the scores from 
+                             //      all the games played.
+    double averageScore;     // Average score, computed by dividing
+                             //      sumOfScores by gamesPlayed.
+    boolean playAgain;       // Record user's response when user is 
+                             //   asked whether he wants to play 
+                             //   another game.
+    
+    
+
+}
